83d045dbb2933782be18cd57d772db73a7d5e7ae
et3r <eterx33@gmail.com>
2022-09-01 19:30:47 -0500

    Initial commit

---
+++ src/main/java/net/et3r/chilaquiles/Chilaquiles.java
@@ -0,0 +1,16 @@
+package net.et3r.chilaquiles;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Chilaquiles implements ModInitializer {
+
+	public static final String MOD_ID = "chilaquiles";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/et3r/chilaquiles/mixin/ExampleMixin.java
@@ -0,0 +1,16 @@
+package net.et3r.chilaquiles.mixin;
+
+import net.et3r.chilaquiles.Chilaquiles;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		Chilaquiles.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
